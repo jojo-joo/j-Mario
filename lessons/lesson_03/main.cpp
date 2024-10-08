87bddf10f3309c8c50fb4df4986d9463e43ced03
jojo-joo <3759526109@qq.com>
2024-10-08 18:37:50 +0800

    lesson 03

--- lessons/lesson_03/main.cpp
+++ lessons/lesson_03/main.cpp
@@ -1,3 +1,22 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+struct fsdata_file
+{
+	const struct fsdata_file* next;
+	const unsigned char* name;
+	const unsigned char* data;
+	const int len;
+};
+#include "fsdata.h"
+
+#define CUTE_TILED_IMPLEMENTATION
+#include "cute_tiled.h"
+
+#define CUTE_PNG_IMPLEMENTATION
+#include "cute_png.h"
+
 #define SDL_MAIN_HANDLED
 #include "sdl.h"
 
@@ -8,7 +27,47 @@ SDL_Window* m_win = NULL;
 SDL_Renderer* m_ren = NULL;
 SDL_Texture* m_tex = NULL;
 
+void tmj_init(void) {
+	cute_tiled_map_t* map = cute_tiled_load_map_from_memory(data_mario_tmj + 11, sizeof(data_mario_tmj) - 11, NULL);
+	assert(map);
+	printf("map\r\n");
+	printf("- width: %d\r\n", map->width);
+	printf("- height: %d\r\n", map->height);
+	printf("- tilewidth: %d\r\n", map->tilewidth);
+	printf("- tileheight: %d\r\n", map->tileheight);
+	printf("\r\n");
+
+	cute_tiled_layer_t* layer = map->layers;
+	assert(layer);
+	printf("layers\r\n");
+	while (layer) {
+		printf("- layer %d: %s\r\n", layer->id, layer->name);
+		layer = layer->next;
+	}
+	printf("\r\n");
+
+	cute_tiled_tileset_t* tilesets = map->tilesets;
+	assert(tilesets);
+	printf("tilesets\r\n");
+	printf("- image: %s\r\n", tilesets->image);
+	printf("- firstgid: %d\r\n", tilesets->firstgid);
+	printf("- imagewidth: %d\r\n", tilesets->imagewidth);
+	printf("- imageheight: %d\r\n", tilesets->imageheight);
+	printf("- tilewidth: %d\r\n", tilesets->tilewidth);
+	printf("- tileheight: %d\r\n", tilesets->tileheight);
+	printf("\r\n");
+
+	cute_tiled_free_map(map);
+}
+
+void png_init(void) {
+	cp_image_t pngs = cp_load_png("asset/tile_set.png");
+}
+
 int main() {
+	tmj_init();
+	png_init();
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	m_win = SDL_CreateWindow("j-Mario", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, GAME_VIEW_WIDTH, GAME_VIEW_HEIGHT, SDL_WINDOW_SHOWN);
 	m_ren = SDL_CreateRenderer(m_win, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
--- lessons/readme.md
+++ lessons/readme.md
@@ -1,3 +1,4 @@
 # lessons
 - lesson_01: draw canvas on windows by SDL2.
 - lesson_02: draw tile maps with tileset by tiled.
+- lesson_03: decode .tmj file
